bbc76f5f346b092941766bcb3b37c02e160edece
alexahdp <alexahdp@gmail.com>
2018-03-23 00:47:45 +0300

    Удалил лишние файлы

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 
 export default () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+    <h1>Hello world</h1>
   </div>
 );
